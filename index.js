4ef4ee846039867d50161c8f0fbcf1143741065a
ritikraj07 <ritikra3rrr@gmail.com>
2023-06-02 14:05:39 +0530

    new portfolio added

--- index.js
+++ index.js
@@ -1,18 +1,19 @@
 
-import emailjs from '@emailjs/browser';
-let Form = document.querySelector('form')
-Form.addEventListener('onSubmit', sendEmail)
-function sendEmail(e) {
-    e.preventDefault();
-    let name = document.getElementById("sender_name")
-    let email = document.getElementById("sender_email")
-    let subject = document.getElementById("sender_subject")
-    let message = document.getElementById("sender_message")
-    console.log(name, email, subject, message)
+// import emailjs from '@emailjs/browser';
+// let Form = document.querySelector('form')
+// Form.addEventListener('onSubmit', sendEmail)
+// function sendEmail(e) {
+//     e.preventDefault();
+//     let name = document.getElementById("sender_name")
+//     let email = document.getElementById("sender_email")
+//     let subject = document.getElementById("sender_subject")
+//     let message = document.getElementById("sender_message")
+//     console.log(name, email, subject, message)
     // emailjs.sendForm('service_pui2o7f', 'template_ipjzpqg', form.current, 'Tl5CtDg1GHn8shtDl')
     //     .then((result) => {
     //         console.log(result.text);
     //     }, (error) => {
     //         console.log(error.text);
     //     });
-}
+// }
+
